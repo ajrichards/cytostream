5bef08e6063e796d7c9069a40d54a0d79c60ce0d
Adam J Richards <ajrichards@users.noreply.github.com>
2010-08-27 17:34:19 -0400

    small changes to get unittests working again

--- unittests/TestCase1.py
+++ unittests/TestCase1.py
@@ -15,46 +15,68 @@ BASEDIR = os.path.dirname(__file__)
 
 ## test class for the main window function
 class TestCase1(unittest.TestCase):
-    def setUp(self):
-        try:
-            self.controller
-        except:
-            self._initialize()
     
-    def _initialize(self):
+    def testTestCase1(self):
+        self.projectID = 'utest'
+        self.allFiles = [os.path.join(BASEDIR,"..","cytostream","example_data", "3FITC_4PE_004.fcs")]
+        self.subsample = '1e3'
         self.controller = Controller()
-        self.controller.initialize_project("utest") 
-        self.failIf(os.path.isfile(os.path.join(BASEDIR,"..","cytostream","example_data", "3FITC_4PE_004.fcs")) == False)
-        self.fcsFileName = os.path.join(BASEDIR,"..","cytostream","example_data", "3FITC_4PE_004.fcs")
+        self.controller.initialize_project(self.projectID)
 
-        if os.path.isfile(self.fcsFileName) == False:
-            print "ERROR: fcsFileName is not true"
+        firstFile = True
+        goFlag = True
+        for fileName in self.allFiles:
+            if os.path.isfile(fileName) == False:
+                print 'ERROR: Bad file name skipping', fileName
+                continue
 
-        self.controller.log.log['selectedFile'] = os.path.split(self.fcsFileName)[-1]
-        self.controller.create_new_project(self.fcsFileName)
-        
-        # subsampling
-        self.controller.log.log['subsample'] = '1e3'
+            fileName = str(fileName)
+            if firstFile == True:
+                self.controller.create_new_project(fileName)
+                firstFile = False
+            else:
+                goFlag = self.controller.load_additional_fcs_files(fileName)
+
+        if self.controller.homeDir == None:
+            print "ERROR: project failed to initialize"
+            return
+        else:
+            print "project created."
+
+        # handle subsampling 
+        self.controller.log.log['subsample'] = self.subsample
         self.controller.handle_subsampling()
+        self.controller.save()
 
-        # check image creation
+        # create quality assurance images
         self.controller.process_images('qa')
+
+        ## run models
+        for fileName in self.allFiles:
+            self.failIf(os.path.isfile(fileName) == False)
+            self.controller.log.log['selectedFile'] = os.path.split(fileName)[-1]
+            print 'running model on', self.controller.log.log['selectedFile']
+            self.runSelectedModel()
+
+        ## create figures 
+        print 'creating results images'
+        self.controller.process_images('results')
 
     def verifyModelRun(self,modelName,modelType):
        statModel,statModelClasses = self.controller.model.load_model_results_pickle(modelName,modelType)    
        return statModelClasses
 
-    def testRunSelectedModel(self):
+    def runSelectedModel(self):
         # numcomponents
         self.controller.log.log['numComponents'] = 16
         self.controller.log.log['modelToRun'] = 'dpmm'
         self.controller.run_selected_model()
-        modelComponents = "%s_sub1000_dpmm"%os.path.split(self.fcsFileName)[-1][:-4]
-        modelModes = "%s_sub1000_dpmm"%os.path.split(self.fcsFileName)[-1][:-4]
-        classesComponents = self.verifyModelRun(modelComponents,'components')
-        classesModes = self.verifyModelRun(modelModes,'modes')
-        self.assertEqual(len(classesComponents),1000)
-        self.assertEqual(len(classesModes),1000)
+        selectedFile = self.controller.log.log['selectedFile']
+        modelName = "%s_sub%s_dpmm"%(re.sub("\.fcs|\.pickle","",selectedFile),int(float(self.subsample)))
+        classesComponents = self.verifyModelRun(modelName,'components')
+        classesModes = self.verifyModelRun(modelName,'modes')
+        self.assertEqual(len(classesComponents),int(float(self.subsample)))
+        self.assertEqual(len(classesModes),int(float(self.subsample)))
 
         # check image creation
         self.controller.process_images('results')
