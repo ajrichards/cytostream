88a5f9afc13ef9d6986969fe494a968939f50932
adamricha <adamricha@gmail.com>
2010-11-01 12:21:42 -0400

    adding 4 plot to cytostream

--- cytostream/tools/__init__.py
+++ cytostream/tools/__init__.py
@@ -1,4 +1,5 @@
 from FileAligner import FileAligner
 from PieChartCreator import PieChartCreator
+from DotPlotCreator import DotPlotCreator
 from MiscFns import calculate_intercluster_score
 from PlottingFns import make_plots_as_subplots, make_scatter_plot
