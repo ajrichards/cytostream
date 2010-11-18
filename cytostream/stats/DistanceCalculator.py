a627b337f413de1338446873fb87771ef325b176
Adam J Richards <ajrichards@users.noreply.github.com>
2010-11-18 09:50:00 -0500

    full integration with mahalanobis distance

--- cytostream/stats/DistanceCalculator.py
+++ cytostream/stats/DistanceCalculator.py
@@ -4,6 +4,7 @@ import time
 import numpy as np
 from scipy.spatial.distance import cdist, squareform
 from scipy.linalg import inv
+from scipy.cluster.vq import whiten
 
 class DistanceCalculator():
     def __init__(self,matrixMeans=None,distType='euclidean'):
@@ -22,36 +23,6 @@ class DistanceCalculator():
             raise RuntimeError("ERROR in distance calculator - input distance type is invalid \nmust be in %s"%validMetrics)
             return None
     
-    def calculate_dev(self,mat,matrixMeans=None,inverseCov=None):
-        ## error checking
-        if type(np.array([])) != type(mat):
-            raise RuntimeError("ERROR in distance calculator - input matrix must be of type np.array()")
-            return None
-        
-        ## gather dimensions of the input matrix
-        dims = mat.shape
-        
-        if len(dims) == 1:
-            n = dims[0]
-            d = 1
-        elif len(dims) == 2:
-            n,d = dims
-        else:
-            raise RuntimeError("ERROR in distance calculator - input matrix does not have reasonable dimensions - %s"%dims)
-
-        if matrixMeans != None:
-            if matrixMeans.size != d:
-                raise RuntimeError("ERROR in distance calculator - badly formated matrix means - %s")
-        else:
-            matrixMeans = mat.mean(axis=0)
-
-        if self.distType == 'euclidean':
-            dists = (mat - matrixMeans)**2.0
-            dists = np.sqrt(dists.sum(axis=1))
-        
-        self.dists = dists
-
-
     def calculate(self,mat,matrixMeans=None,inverseCov=None):
         ## error checking
         if type(np.array([])) != type(mat):
@@ -83,7 +54,11 @@ class DistanceCalculator():
         if self.distType == 'euclidean':
             self.dists = cdist(mat,matrixMeans,self.distType)
         elif self.distType == 'mahalanobis':
-            self.dists = cdist(mat,matrixMeans,self.distType, VI=None)
+            try:
+                self.dists = cdist(mat,matrixMeans,self.distType, VI=None)
+            except:
+                self.dists = cdist(mat,matrixMeans,'euclidean')
+                self.dists = whiten(self.dists)
 
         self.dists = self.dists[:,0]
 
@@ -119,10 +94,17 @@ class DistanceCalculator():
 
         ## get the covariance matrix and its inverse
         cov = np.cov(a)
-        invCov = np.linalg.inv(cov)
+        try:
+            invCov = np.linalg.inv(cov)
+        except:
+            print "WARNING: inverse of covariance failed -- matrix is singlular"
+            return None
 
         if invCov.shape[0] != d and invCov.shape[1] != d:
             raise RuntimeError("ERROR in distance calculator - input matrix does not have reasonable dimensions - %s"%dims)
+
+        return invCov
+
 
     def get_distances(self):
         return self.dists
