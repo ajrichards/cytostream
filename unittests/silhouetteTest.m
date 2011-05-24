87111c65546c582dcaa2d756024d964764d0b2d1
Adam J Richards <ajrichards@users.noreply.github.com>
2011-05-24 15:19:57 -0400

    adding unitestfor silhouette ind

---
+++ unittests/silhouetteTest.m
@@ -0,0 +1,15 @@
+%% silhouette value unittest
+
+x = [2,2,3,4,5,6,7,8,9,7];
+y = [5,4,3,5,3,9,9,8,7,7];
+
+
+
+scatter(x,y)
+xlim([0,12]);
+ylim([0,12]);
+
+X = [x;y].'
+X
+cidx = kmeans(X, 2, 'distance', 'sqeuclid')
+s = silhouette(X, cidx, 'sqeuclid')
