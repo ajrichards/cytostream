b373666f253d27b99911c57d1d8ce0e48f803e77
Adam J Richards <ajrichards@users.noreply.github.com>
2012-04-11 16:55:27 -0400

    solved qitemmodel qitemdelegate by column issue

--- cytostream/Main.py
+++ cytostream/Main.py
@@ -14,13 +14,31 @@ if mpl.get_backend() != 'agg':
     mpl.use('agg')
 from cytostream.qtlib import MainWindow
 
+
+## check for the debug flag
+try:
+    optlist, args = getopt.getopt(sys.argv[1:],'p:d')
+except getopt.GetoptError:
+    print getopt.GetoptError
+    print sys.argv[0] + "-p"
+    print "Note: project (-p) flag to run the software with a given project"
+    sys.exit()
+
+debug = False
+projectID = None
+for o, a in optlist:
+    if o == '-p':
+        projectID = a
+    if o == '-d':
+        debug = True
+
 class Main():
     def __init__(self):
         app = QtGui.QApplication(sys.argv)
         app.setOrganizationName("Duke University")
         app.setOrganizationDomain("duke.edu")
         app.setApplicationName("cytostream")
-        mw = MainWindow()
+        mw = MainWindow(debug=debug,projectID=projectID)
         mw.show()
         app.exec_()
 
