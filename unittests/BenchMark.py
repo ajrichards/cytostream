fed1d4adcb606af7da171716e55277c01a3a2023
A Richards <A Richards@localhost>
2012-05-16 18:22:43 -0400

    updated BenchMark.py to help better debug and pass FCM errors

--- unittests/BenchMark.py
+++ unittests/BenchMark.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 
-import sys,os,unittest,time,re,time
+import sys,os,unittest,time,re,time,subprocess
 
 import matplotlib as mpl
 if mpl.get_backend() != 'agg':
@@ -9,25 +9,34 @@ if mpl.get_backend() != 'agg':
 
 from cytostream import NoGuiAnalysis
 
+## python path 
+if sys.platform == 'win32':
+    pythonPath = os.path.join("C:\Python27\python")
+elif sys.platform == 'darwin':
+    pythonPath = os.path.join("/","usr","local","bin","python")
+else:
+    pythonPath = os.path.join(os.path.sep,"usr","bin","python")
+
 ## test class for the main window function
 class BenchMarkTest(unittest.TestCase):
     def setUp(self):
         cwd = os.getcwd()
         if os.path.split(cwd)[1] == 'unittests':
-            BASEDIR = os.path.split(cwd)[0]
+            self.baseDir = os.path.split(cwd)[0]
         elif os.path.split(cwd)[1] == 'cytostream':
-            BASEDIR = cwd
+            self.baseDir = cwd
         else:
             print "ERROR: Model test cannot find home dir -- cwd", cwd
 
         ## declare variables
         projectID = 'utest'
-        homeDir =  os.path.join(BASEDIR,"cytostream","projects", projectID)
-        filePathList = [os.path.join(BASEDIR,"cytostream","example_data", "3FITC_4PE_004.fcs")]
+        self.homeDir =  os.path.join(self.baseDir,"cytostream","projects", projectID)
+        filePathList = [os.path.join(self.baseDir,"cytostream","example_data", "3FITC_4PE_004.fcs")]
+        self.fileName = '3FITC_4PE_004'
         channelDict = {'FSCH':0,'SSCH':1,'FL1H':2,'FL2H':3}
         
         ## run the initial model for all files
-        self.nga = NoGuiAnalysis(homeDir,channelDict,filePathList,useSubsample=True,makeQaFigs=False,record=False)
+        self.nga = NoGuiAnalysis(self.homeDir,channelDict,filePathList,useSubsample=True,makeQaFigs=False,record=False)
         self.nga.set('subsample_qa', 1000)
         self.nga.set('subsample_analysis', 1000)
         self.nga.set('model_to_run','dpmm-mcmc')
@@ -42,8 +51,20 @@ class BenchMarkTest(unittest.TestCase):
             print "ERROR: gpu not available not running benchmark"
             return
 
-        timeBegin = time.time()        
-        self.nga.run_model()
+        timeBegin = time.time()
+        #self.nga.run_model()
+        gpuDevice = 0
+        script = os.path.join(self.baseDir,'cytostream',"RunModelDPMM.py")
+        cmd = "%s %s -h %s -g %s -f %s"%(pythonPath,script,self.homeDir,gpuDevice,self.fileName)
+        proc = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stdin=subprocess.PIPE)
+
+        while True:
+            next_line = proc.stdout.readline()
+            if next_line == '' and proc.poll() != None:
+                break
+            sys.stdout.write(next_line)
+            sys.stdout.flush()
+
         timeEnd = time.time()
         runTime = timeEnd - timeBegin
         print "Runtime", runTime
